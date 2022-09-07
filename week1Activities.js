cddbd7c2d04184c5a90889b2ae5ddcd66f93e027
JRobbo85 <jrobertson85@hotmail.co.uk>
2022-09-07 11:13:37 +0100

    Week 1 Activities

---
+++ week1Activities.js
@@ -0,0 +1,106 @@
+// //Task 2
+
+// const pet = {
+//     petName: "Doggo",
+//     typeOfPet: "Dog",
+//     age: 1,
+//     colour: "Brown",
+
+//     eat(){
+//         return `${this.petName} is eating `
+//     },
+//     drink(){
+//         return `${this.petName} is drinking`
+//     }
+// }
+
+// console.log(pet.eat())
+// console.log(pet.drink())
+
+// // Task 5
+
+// let num = 0
+
+// for (let i = 0; i < 6; i++){
+//     if (i <= 6){
+//         num = Math.ceil(Math.random()*30)
+//         if (num % 7 == 0){
+//             console.log(`${num} is divisible by 7`)
+//         }
+//         else{
+//             console.log(`${num} is not divisible by 7`)
+//         }
+//     }
+// }
+
+// //Task 1
+
+// const person = {
+//     name: "John",
+//     age: 37,
+
+//     sayHi(){
+//         return `Hi, my name is ${this.name}`
+//     }
+
+// }
+
+// person.faveSongs =  ["Tornado of Souls", "Unforgiven", "Painkiller", "Somebody Someone" ]
+
+// console.log(person.sayHi())
+
+// //Task 2
+
+// let num = 0
+
+// for (let i = 0; i < 6; i++){
+//     if (i <= 6){
+//         num = Math.ceil(Math.random()*50)
+//         console.log(num)
+//     }
+// }
+
+// //Task 3
+
+// for(let i = 9; i >= 0; i--){
+//     if(i >= 0){
+//         console.log(i)
+//     }
+// }
+
+// // Task 2
+
+// let person = {
+//     userName: "John",
+//     breakfast: "bacon",
+//     lunch: "coffee",
+//     dinner: "pizza"
+// }
+
+// console.log(`My name is ${person.userName} and for breakfast i had ${person.breakfast}, for lunch i had ${person.lunch} and for dinner i had ${person.dinner}`)
+
+
+// //Task 1
+
+// let films = [
+//     "Godfather",
+//     "Alien",
+//     "Resevoir Dogs",
+//     "Pulp Fiction",
+//     "Ghostbusters"
+// ]
+
+// console.log(films)
+
+// films.push("The Thing", "se7en")
+
+// console.log(films)
+
+// arrayCount = films.length
+// console.log(arrayCount)
+
+// for (let i = 0; i < arrayCount; i++){
+//     if (i <= arrayCount){
+//         console.log(films[i])
+//     }
+// }
